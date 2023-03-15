867415c01de4e3cd6119dccf3e7ab8c3a86dc663
JamesSong12 <jimin99511@gmail.com>
2023-03-15 20:51:03 +0900

    첫 씬 생성

--- Song_project/Assets/Scripts/Test.cs
+++ Song_project/Assets/Scripts/Test.cs
@@ -7,9 +7,11 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int age;
+        /*int age;
         age = 30;
-        Debug.Log(age);
+        Debug.Log(age);*/
+
+        
     }
 
     // Update is called once per frame
---
+++ Song_project/Assets/Scripts/Test2.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test2 : MonoBehaviour
+{
+    public int hp = 180;
+    public Text textHpUI;              // HP UI 표시 선언
+    public Text textStateUI;           // State 표시 선언
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // =======================================================
+        // UI 변하게하는 원동력
+        // =======================================================
+
+        if (Input.GetMouseButtonDown(0)) // 왼쪽 버튼 마우스
+        {
+            hp += 10;
+        }
+        if (Input.GetMouseButtonDown(1)) // 오른쪽 버튼 마우스
+        {
+            hp -= 10;
+        }
+
+        // =======================================================
+        // UI 움직임
+        // =======================================================
+
+        textHpUI.text = hp.ToString();
+        if (hp <= 50)
+        {
+            textStateUI.text = "Run!!";
+        }
+        else if (hp >= 200)
+        {
+            textStateUI.text = "Attack";
+        }
+        else
+        {
+            textStateUI.text = "Defance!!";
+        }
+       
+
+    }
+}
